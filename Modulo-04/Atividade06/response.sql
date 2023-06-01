944936fd91432d5735e599517c271b80295fc495
Regis Amaral <amaral.regis@gmail.com>
2023-05-31 21:15:46 -0300

    Update response.sql

--- Modulo-04/Atividade06/response.sql
+++ Modulo-04/Atividade06/response.sql
@@ -1,32 +1,19 @@
--- Calcule o valor total das comandas;
+--### Atividade 6
+--Utilizando a base de dados do projeto Ravin, construa as seguintes queries:
+--1. Calcule o valor total das comandas;
 SELECT sum(c.valor) AS "Valor Total das Comandas" FROM comanda c;
--- Obtenha o preço de venda máximo dos produtos;
+--2. Obtenha o preço de venda máximo dos produtos;
 SELECT max(p.precovenda) AS "Maior Preço de Venda" FROM produto p;
--- Encontre o tempo de preparo mínimo dos produtos;
+--3. Encontre o tempo de preparo mínimo dos produtos;
 SELECT MIN(p.tempodepreparo) AS "Tempo de Preparo Mínimo" FROM produto p;
--- Calcule a média dos preços de custo dos produtos;
+--4. Calcule a média dos preços de custo dos produtos;
 SELECT avg(p.precocusto) AS "Média dos Preços de Custo"  FROM produto p;
--- Conte quantas mesas estão disponíveis;
+--5. Conte quantas mesas estão disponíveis;
 SELECT count(m.id) AS "Total de Mesas Disponíveis" FROM mesa m INNER JOIN statusmesa s ON s.id = m.statusmesaid WHERE s.nome = 'Livre'; 
--- Calcule o valor total de vendas por comanda;
-SELECT c.codigo AS "Comanda", SUM(c.valor) AS "Total" 
-FROM comanda c 
-INNER JOIN comandaproduto c2 ON c2.comandaid = c.id 
-GROUP BY c.codigo 
-ORDER BY c.codigo;
--- Obtenha o preço de venda máximo por categoria de produto;
-
--- Encontre a quantidade máxima de produtos vendidos por comanda;
-SELECT c.codigo AS "Comanda", MAX(c2.quantidade) AS "Quantidade Máxima de Produtos"  
-FROM comanda c 
-INNER JOIN comandaproduto c2 ON c2.comandaid = c.id 
-GROUP BY c.codigo 
-ORDER BY c.codigo;
--- Calcule a média de valor total por comanda;
-SELECT c.codigo AS "Comanda", AVG(c2.valortotal) AS "Média dos Valores por Comanda"  
-FROM comanda c 
-INNER JOIN comandaproduto c2 ON c2.comandaid = c.id 
-GROUP BY c.codigo 
-ORDER BY c.codigo;
--- Conte quantos produtos ativos existem;
+--6. Calcule o valor total de vendas das comanda com status “Paga”;
+SELECT SUM(c.valor) AS "Valor total das comandas" FROM comanda c WHERE c.statuscomandaid = 3;
+--7. Obtenha o preço de venda máximo dos produtos;
+--8. Encontre o o maior valor de uma determinada comanda;
+--9. Calcule a média de valor total das comanda;
+--10. Conte quantos produtos ativos existem;
 SELECT count(p.id) AS "Quantidade de Produtos Ativos" FROM produto p WHERE p.ativo IS TRUE;
